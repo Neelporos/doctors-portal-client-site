09cc59e53f8f4d00bce1c8afa70db3283afa4ea4
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-10 07:23:40 -0700

    Service Section Done

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -7,17 +7,17 @@ const Banner = () => {
       
     <div>
         
-      <div class="hero min-h-screen px-12"  style={{backgroundImage: `url(${bg})`}}>
-        <div class="hero-content flex-col lg:flex-row-reverse">
-          <img src={chair} class="max-w-sm rounded-lg shadow-2xl" />
+      <div className="hero min-h-screen"  style={{backgroundImage: `url(${bg})`}}>
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
           <div>
-            <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
-            <p class="py-6">
+            <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
+            <p className="py-6">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the
             </p>
-            <button class="btn btn-secondary uppercase text-white bg-gradient-to-r from-secondary to-primary font-bold">Get Started</button>
+            <button className="btn btn-secondary uppercase text-white bg-gradient-to-r from-secondary to-primary font-bold">Get Started</button>
           </div>
         </div>
       </div>
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -1,46 +1,71 @@
-import React from 'react';
-import fluoride from '../../assets/images/fluoride.png';
-import cavity from '../../assets/images/cavity.png';
-import whitening from '../../assets/images/whitening.png';
-import Service from './Service';
+import React from "react";
+import fluoride from "../../assets/images/fluoride.png";
+import cavity from "../../assets/images/cavity.png";
+import whitening from "../../assets/images/whitening.png";
+import treatment from "../../assets/images/treatment.png";
+import Service from "./Service";
 
 const Services = () => {
-    const services =[
-        {
-            _id: 1,
-            name: 'Fluoride Treatment',
-            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the', 
-            img: fluoride
-        },
-        {
-            _id: 2,
-            name: 'Cavity Filling',
-            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the', 
-            img: cavity
-        },
-        {
-            _id: 3,
-            name: 'Teeth Whitening',
-            description: 'Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the', 
-            img: whitening
-        }
-    ]
-    return (
-        <div className='my-28'>
-            <div className='text-center'>
-                <h2 className='text-secondary font-bold text-xl uppercase'>Our Services</h2>
-                <h2 className='text-4xl'>Services We Provide</h2>
+  const services = [
+    {
+      _id: 1,
+      name: "Fluoride Treatment",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: fluoride,
+    },
+    {
+      _id: 2,
+      name: "Cavity Filling",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: cavity,
+    },
+    {
+      _id: 3,
+      name: "Teeth Whitening",
+      description:
+        "Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the",
+      img: whitening,
+    },
+  ];
+  return (
+    <div className="my-28">
+      <div className="text-center">
+        <h2 className="text-secondary font-bold text-xl uppercase">
+          Our Services
+        </h2>
+        <h2 className="text-4xl">Services We Provide</h2>
+      </div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-28">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
+      <div>
+        <div className="hero min-h-screen">
+          <div className="hero-content flex-col lg:flex-row">
+            <img
+              src={treatment}
+              className="max-w-sm rounded-lg shadow-2xl"
+            />
+            <div>
+              <h1 className="text-5xl font-bold">Exceptional Dental Care, on Your Terms</h1>
+              <p className="py-6">
+              It is a long established fact that a reader will be distracted by 
+              the readable content of a page when looking at its layout. 
+              The point of using Lorem Ipsumis that it has a more-or-less normal 
+              distribution of letters,as opposed to using 'Content here, content here', 
+              making it look like readable English. 
+              Many desktop publishing packages and web page
+              </p>
+              <button className="btn btn-secondary uppercase text-white bg-gradient-to-r from-secondary to-primary font-bold">Get Started</button>
             </div>
-            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-28'>
-                {
-                    services.map(service => <Service
-                    key={service._id}
-                    service={service}
-                    ></Service>)
-                }
-            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Services;
