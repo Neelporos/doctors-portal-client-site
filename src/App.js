5117abff9fd764a9234aa38b16b37d39866ad0b8
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-11 12:05:52 -0700

    Footer Done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Contact from './Pages/Contact/Contact';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Reviews from './Pages/Reviews/Reviews';
+import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path='/login' element={<Login></Login>}></Route>
           <Route path='/about' element={<About></About>}></Route>
         </Routes>
+        <Footer></Footer>
     </div>
   );
 }
