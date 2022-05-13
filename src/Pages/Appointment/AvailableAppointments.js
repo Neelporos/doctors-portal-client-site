5bc0ab4588693b7b982baa9e1d156040d4d41751
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-12 23:38:50 -0700

    Make service Section Done

--- src/Pages/Appointment/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments.js
@@ -1,10 +1,26 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from './Service'
 
 const AvailableAppointments = ({date}) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
     return (
-        <div>
+        <div className='mt-10'>
             <p className='text-xl text-center text-primary'>Available Appointments on: {format(date, "PP")}</p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10'>
+                {
+                    services.map(service => <Service 
+                    key={service._id}
+                    service={service}
+                    ></Service>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/Pages/Appointment/Service.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const service = ({ service }) => {
+  const { name, slots } = service;
+  return (
+    <div>
+      <div className="card lg:w-msx-w-lg bg-base-100 shadow-xl mt-10">
+        <div className="card-body items-center text-center">
+          
+          <h2 className="card-title">{name}</h2>
+          <p>{
+             slots.length > 0
+             ? <span>{slots.length[0]}</span>
+             : <span className="text-red-500">No Space Available Try Another Day</span>
+             }
+          </p>
+          <p>
+            {slots.length} {slots.length > 1 ? "spaces" : "space"} Available
+          </p>
+          <div className="card-actions">
+            <button className="btn btn-secondary uppercase text-white bg-gradient-to-r from-secondary to-primary font-bold">Book Appointment</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default service;
